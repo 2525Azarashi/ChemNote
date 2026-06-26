f6cd03ec24a46c9ce864f7355d66f05871735508
2525Azarashi <s.yutoneko7@gmail.com>
2026-06-26 22:00:40 +0900

    Update index.ts

--- src/data/learningContent/index.ts
+++ src/data/learningContent/index.ts
@@ -1,1 +1,9 @@
+// Auto-generated barrel for learning content sections
+export { LEARNING_GLOBAL_CSS } from './globalCss';
+export { SECTION_1_1_HTML } from './section_1_1';
+export { SECTION_1_2_HTML } from './section_1_2';
+export { SECTION_1_3_HTML } from './section_1_3';
+export { SECTION_2_1_HTML } from './section_2_1';
+export { SECTION_2_2_HTML } from './section_2_2';
+export { SECTION_2_3_HTML } from './section_2_3';
 
