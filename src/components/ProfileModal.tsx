66840621f8a88d7b756b1c5a6bb6227ca151727b
Yuto Shoji <s.yutoneko7@gmail.com>
2026-06-27 14:09:43 +0900

    Update ProfileModal.tsx

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -300,7 +300,7 @@ export function ProfileModal({ onClose, isBgmEnabled, setIsBgmEnabled, bgmVolume
                 </button>
               </>
             )}
-          </div>}
+          </div>
         </div>
       </div>
     </div>
